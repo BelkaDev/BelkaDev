d62426541d103784fd9bc88b03b001ac400c714d
Belka <belk5@outlook.com>
2022-06-20 14:19:29 +0100

    Automated publish by Azure: Mon Jun 20 2022

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const fetch = require('node-fetch');
 request = require('request');
 const XMLHttpRequest = require('xhr2');
 
-/*  function download (uri, filename, callback){
-   console.log('**********************')
-  console.log('URI:'+uri.toString())
+ function download (uri, filename, callback){
   request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
-}; */
+};
 function getCurrentSong(callback) {
     const that = this
     const url =
